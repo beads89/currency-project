35142097721ff69c490e4393e5392e9aa31d1cee
Brandon Eads <brandon181989@hotmail.com>
2021-09-10 10:16:48 -0700

    write conversionOutput with placeholder in 'else if'

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ function clearFields() {
   // etc., etc.
 }
 
+function conversionOutput(response, currencyOne, currencyTwo, currencyAmount) {
+  if (currencyAmount <= 0) {
+    $("#invalidInput").text("Please enter an amount greater than 0.00");
+  } else if (response.result === "success") {
+    $("#REPLACE").text(`Something with ${currencyAmount} ${currencyOne} converts to ${response.conversion_result} ${currencyTwo} and possibly adding that it's a rate of 1 ${currencyOne} to ${response.conversion_rate} ${currencyTwo}. `)
+  } else {
+    $("#error").text(`There was an error: ${response.result} ${response.error-type}`)
+  }
+}
+
+
 async function makeApiCall(currencyOne, currencyTwo, currencyAmount) {
   const response = await Currency.conversionRate(currencyOne, currencyTwo, currencyAmount);
+  conversionOutput(response, currencyOne, currencyTwo, currencyAmount);
 }
