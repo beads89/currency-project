c4c1283cda126f5166ca56823a45e9fce133e2d8
Brandon Eads <brandon181989@hotmail.com>
2021-09-10 08:30:07 -0700

    fix file formatting

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { ClassExample1, ClassExample2 } from './project';
+import { ClassExample1, ClassExample2 } from './services/project';
